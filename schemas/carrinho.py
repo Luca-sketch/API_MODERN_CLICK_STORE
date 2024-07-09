c65bd9b5ab61906023c804b6c0a99f5d2ead60ec
Luca-sketch <79028489+Luca-sketch@users.noreply.github.com>
2024-07-09 16:36:43 -0300

    Delete schemas directory

--- schemas/carrinho.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-# Schema para adicionar um novo item ao carrinho
-class CarrinhoSchema(BaseModel):  
-    Produto: str
-    Valor: str
-    Onda: str
-  
-
--- schemas/error.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-# Schema para lidar com os erros
-class ErrorSchema(BaseModel):
-    """ Define como uma mensagem de erro será representada """
-    message: str
